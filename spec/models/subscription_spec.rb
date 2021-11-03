2d8c73b0dc2dac9982e051fbe61a634383368667
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-03 00:39:50 -0700

    Add subscriptions model

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
   describe 'validations' do
+    it { should belong_to(:customer) }
+    it { should belong_to(:tea) }
+
     it { should validate_presence_of(:title)}
     it { should validate_presence_of(:price)}
     it { should validate_presence_of(:status)}
