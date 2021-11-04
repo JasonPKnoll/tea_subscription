f27077ceac16d5597dabf0aab9325daeedc4e78c
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-04 02:51:53 -0700

    Refactpr create method in subscriptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
   def create
-    customer = Customer.find_by_id(params[:customer])
+    customer = Customer.find_by_id(params[:customer_id])
     @tea = Tea.find_by(title: params[:tea])
     if customer.present? && @tea.present?
-      subscription = customer.subscriptions.create(subscription_params)
+      subscription = customer.subscriptions.create(subscription_params.merge(tea_id: @tea.id))
       render json: SubscriptionSerializer.new(subscription), status: 201
     else
       render json: {:error => 'Record not found'}, status: 404
@@ -20,7 +20,7 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   private
   def subscription_params
-    params.require(:subscription).permit(:title, :price, :status, :frequency).merge(tea_id: @tea.id)
+    params.permit(:title, :price, :status, :frequency)
   end
 
 end
